ba2b9a1c6074d79b6fd27b33ad0a6aabac63f298
luizxrs <lgustavoitp@gmail.com>
2023-08-03 22:50:40 +0100

    Feat: Mobile Responsiviness, Componentization

--- src/components/QuemSomos/page.tsx
+++ src/components/QuemSomos/page.tsx
@@ -3,16 +3,13 @@
 import Image from "next/image";
 import { useRef, useEffect, useState } from "react";
 import { motion } from "framer-motion";
-import { twMerge } from "tailwind-merge";
-
-import BeachResidenceLogo from "@/assets/svg/beach-residence-logo.svg";
-import images from "@/images";
 import members from "@/app/quemsomos/members";
 import TextSlideBar from "./TextSlideBar";
 import FounderCard from "./FounderCard";
 import founderCards from "@/app/quemsomos/founders";
-import { IFounderCard } from "@/app/quemsomos/founders";
 import OutlineTitle from "./OutlineTitle";
+import ImageStack from "./ImageStack";
+import AnimatedText from "./AnimatedText";
 
 export default function QuemSomosPage() {
   const [dragLeftConstraint, setDragLeftConstraint] = useState(0);
@@ -32,138 +29,35 @@ export default function QuemSomosPage() {
         <section className="max-w-screen-2xl h-screen pb-24 relative">
           {/* FIRST */}
           <div className=" max-w-screen-xl m-auto h-full ">
-            <div className="flex flex-col gap-2 my-12 justify-center items-center h-full w-full">
-              <h1 className="text-8xl font-bold pointer-events-none text-center z-20 w-full">
+            <div className="flex flex-col gap-2 mx-auto my-12 justify-center items-center h-full w-[90%] md:w-full">
+              <h1 className="text-4xl md:text-6xl lg:text-8xl font-bold pointer-events-none text-center z-20 w-full">
                 CONSTRUÍMOS <br /> HÁ 16 ANOS O{" "}
                 <span className="text-Primary-100">FUTURO</span> <br /> DA SUA
                 FAMÍLIA
               </h1>
-              <h1 className="text-center pb-20 text-4xl font-bold pointer-events-none absolute scale-[4] self-start left-1/2 -translate-x-1/2 font-outline-1 text-transparent">
+              <OutlineTitle>
                 SOBRE
                 <br />
                 NÓS
-              </h1>
+              </OutlineTitle>
             </div>
           </div>
-          <div className="w-full left-1/2 -translate-x-1/2 absolute top-0 bottom-0 grid grid-cols-3 gap-12 opacity-10 overflow-hidden">
-            <div className="flex flex-col gap-12">
-              <div className="w-full h-96 bg-slate-200">
-                <Image
-                  src={BeachResidenceLogo}
-                  alt="image"
-                  width={500}
-                  height={900}
-                />
-              </div>
-              <div className="w-full h-96 bg-slate-200">
-                <Image
-                  src={BeachResidenceLogo}
-                  alt="image"
-                  width={500}
-                  height={900}
-                />
-              </div>
-              <div className="w-full h-96 bg-slate-200">
-                <Image
-                  src={BeachResidenceLogo}
-                  alt="image"
-                  width={500}
-                  height={900}
-                />
-              </div>
-              <div className="w-full h-96 bg-slate-200">
-                <Image
-                  src={BeachResidenceLogo}
-                  alt="image"
-                  width={500}
-                  height={900}
-                />
-              </div>
-            </div>
-            <div className="flex flex-col gap-12">
-              <div className="w-full h-96 bg-slate-200">
-                <Image
-                  src={BeachResidenceLogo}
-                  alt="image"
-                  width={500}
-                  height={900}
-                />
-              </div>
-              <div className="w-full h-96 bg-slate-200">
-                <Image
-                  src={BeachResidenceLogo}
-                  alt="image"
-                  width={500}
-                  height={900}
-                />
-              </div>
-              <div className="w-full h-96 bg-slate-200">
-                <Image
-                  src={BeachResidenceLogo}
-                  alt="image"
-                  width={500}
-                  height={900}
-                />
-              </div>
-              <div className="w-full h-96 bg-slate-200">
-                <Image
-                  src={BeachResidenceLogo}
-                  alt="image"
-                  width={500}
-                  height={900}
-                />
-              </div>
-            </div>
-            <div className="flex flex-col gap-12">
-              <div className="w-full h-96 bg-slate-200">
-                <Image
-                  src={BeachResidenceLogo}
-                  alt="image"
-                  width={500}
-                  height={900}
-                />
-              </div>
-              <div className="w-full h-96 bg-slate-200">
-                <Image
-                  src={BeachResidenceLogo}
-                  alt="image"
-                  width={500}
-                  height={900}
-                />
-              </div>
-              <div className="w-full h-96 bg-slate-200">
-                <Image
-                  src={BeachResidenceLogo}
-                  alt="image"
-                  width={500}
-                  height={900}
-                />
-              </div>
-              <div className="w-full h-96 bg-slate-200">
-                <Image
-                  src={BeachResidenceLogo}
-                  alt="image"
-                  width={500}
-                  height={900}
-                />
-              </div>
-            </div>
-            <div></div>
-            <div></div>
+          <div className="w-full left-1/2 -translate-x-1/2 absolute top-0 bottom-0 grid grid-cols-1 sm:grid-cols-2  md:grid-cols-3 gap-12 opacity-10 overflow-hidden">
+            <ImageStack order={0} directionUp={false} />
+            <ImageStack order={0} directionUp={true} />
+            <ImageStack order={1} directionUp={false} />
           </div>
         </section>
 
         <section className="w-screen mb-24">
-          <div className=" max-w-screen-2xl m-auto s">
+          <div className=" max-w-screen-2xl">
             <div className="w-full overflow-hidden">
               <TextSlideBar direction={"left"} />
               <TextSlideBar direction={"right"} />
             </div>
-            <div className="w-full h-[900px] pt-48 px-12 relative">
-              <div className="w-full h-full grid grid-cols-2 gap-x-36 px-12 relative">
-                <OutlineTitle>
-                  FOUNDERS
-                </OutlineTitle>
+            <div className="w-full h-[1800px] md:h-[900px] pt-48 px-12 relative">
+              <div className="w-full h-full grid grid-cols-1 md:grid-cols-2 gap-x-24 px-12 relative">
+                <OutlineTitle>FOUNDERS</OutlineTitle>
                 <div className="absolute top-10 bottom-0 right-0 left-0 bg-Primary-300"></div>
                 {founderCards.map((founder) => (
                   <FounderCard
@@ -180,37 +74,30 @@ export default function QuemSomosPage() {
           <div className=" w-full">
             <div className="w-full overflow-hidden">
               <div className="flex flex-col gap-2 my-12 justify-center items-center h-full">
-                <h2 className="text-7xl font-bold pointer-events-none text-center z-20">
+                <h2 className="text-4xl md:text-7xl  font-bold pointer-events-none text-center z-20">
                   MANIFESTO
                 </h2>
-                <motion.p
-                  initial={{ opacity: 0 }}
-                  whileInView={{ opacity: 1 }}
-                  className="text-5xl font-extralight pointer-events-none text-center z-20 w-3/4 leading-snug"
-                >
-                  CONSTRUINDO UM FUTURO MELHOR COM INOVAÇÃO, QUALIDADE E
-                  COMPROMISSO COM NOSSOS CLIENTES E O MEIO AMBIENTE.
-                </motion.p>
+                <AnimatedText
+                  text="CONSTRUINDO UM FUTURO MELHOR COM INOVAÇÃO, QUALIDADE E COM NOSSOS CLIENTES E O MEIO AMBIENTE."
+                />
               </div>
               <div className="w-full pt-48 mb-24 relative">
-                <div className="w-full h-full">
-                  <OutlineTitle>
-                    EQUIPE
-                  </OutlineTitle>
+                <div className="w-[90vw] h-full">
+                  <OutlineTitle>EQUIPE</OutlineTitle>
                   <motion.div
                     ref={carousel}
-                    className="h-full w-full overflow-hidden cursor-grab"
+                    className="h-full w-full overflow-hidden cursor-grab mx-auto"
                   >
                     <motion.div
                       drag="x"
                       dragConstraints={{ right: 0, left: dragLeftConstraint }}
-                      className="h-full grid grid-flow-col gap-x-20 px-6 relative"
+                      className="h-full max-w-full grid grid-flow-col gap-x-20 px-6 relative"
                     >
                       {/* //CARROUESEL */}
                       {members.map((member) => {
                         return (
                           <>
-                            <motion.div className="w-96 h-[36rem] relative group">
+                            <motion.div className="w-[25vw] h-[40vw] relative group">
                               <motion.div className="absolute inset-0 bottom-10 bg-zinc-900/30 z-10 group-hover:opacity-0 transition-opacity duration-700"></motion.div>
                               <motion.div className="absolute top-0 bottom-10 right-0 left-0">
                                 <motion.div className="absolute top-0 bottom-0 right-0 left-0 overflow-hidden">
@@ -220,7 +107,7 @@ export default function QuemSomosPage() {
                                     className="w-full h-full object-cover group-hover:scale-105 transition-transform duration-700"
                                   />
                                 </motion.div>
-                                <motion.div className="w-[80%] h-10 bg-Primary-300 absolute left-1/2 -translate-x-1/2 -bottom-5 z-20 flex justify-center items-center tracking-wide">
+                                <motion.div className="w-[80%] h-10 bg-Primary-300 absolute left-1/2 -translate-x-1/2 -bottom-5 z-20 flex justify-center items-center text-center tracking-wide text-[.75rem] md:text-base">
                                   {member.title}
                                 </motion.div>
                               </motion.div>
